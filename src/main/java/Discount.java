305c9f40bec3ef3c7c0c3c1c95151e15f8f2813e
Vincent <yxia0059@student.monash.edu>
2019-10-03 23:04:42 +1000

    Update discount
    update discount

---
+++ src/main/java/Discount.java
@@ -0,0 +1,20 @@
+public class Discount {
+    String customerType;
+    double discount;
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
